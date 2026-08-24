b00253facb288306f0554a1cd3dd3b028c4b6468
Einsli1 <Einsli1@users.noreply.github.com>
2026-08-24 20:38:40 +0800

    Bump to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.einsli.photoroulette"
         minSdk = 36
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.5"
+        versionCode = 14
+        versionName = "1.6"
     }
     buildFeatures { compose = true; buildConfig = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
